1811d426e28645bbad4c849cc8ff88dc607f8976
Gnauqoa <quanglng2022@gmail.com>
2023-03-13 10:18:06 +0700

    test userCard controller

--- src/controllers/userCard/register.js
+++ src/controllers/userCard/register.js
@@ -3,7 +3,8 @@ import formatUserCardRes from "../../services/formatUserCardRes";
 
 const register = async (req, res) => {
   try {
-    const { first_name, last_name, type, birth, address, email } = req.body;
+    const { first_name, last_name, type, birth, address, email, password } =
+      req.body;
     const existingUser = await UserCardModel.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
@@ -14,9 +15,10 @@ const register = async (req, res) => {
       type,
       birth,
       address,
-      created_at: Date.now(),
+      password: password || "12345678",
     });
     await userCard.save();
+    console.log(userCard);
     res.status(201).json({
       message: "Create user success",
       data: formatUserCardRes(userCard),
