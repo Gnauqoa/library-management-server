ef343fe19e276c8d36572a341c3e9d32cd2a562f
Gnauqoa <quanglng2022@gmail.com>
2023-03-13 12:30:09 +0700

    add controller/addAuthor

---
+++ src/controllers/author/addAuthor.js
@@ -0,0 +1,24 @@
+import Author from "../../models/author.js";
+import formatAuthorRes from "../../services/formatAuthorRes.js";
+
+const addAuthor = async (req, res) => {
+  try {
+    const { first_name, last_name, birth, sex } = req.body;
+    const { date, month, year } = birth;
+    const author = new Author({
+      first_name,
+      last_name,
+      sex,
+      birth: dayjs()["date"](date)["month"](month)["year"](year).toDate(),
+    });
+    await author.save();
+    res.status(201).json({
+      message: "Create author success",
+      data: formatAuthorRes(author),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default addAuthor;
