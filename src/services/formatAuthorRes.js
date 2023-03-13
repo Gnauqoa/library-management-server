42a388c196db55ee0d1b3a43fd1bfcb1917eba98
Gnauqoa <quanglng2022@gmail.com>
2023-03-13 12:30:29 +0700

    add services/formatAuthorRes

---
+++ src/services/formatAuthorRes.js
@@ -0,0 +1,13 @@
+const formatAuthorRes = (userCard) => {
+  return {
+    id: userCard.id,
+    first_name: userCard.first_name,
+    last_name: userCard.last_name,
+    created_at: userCard.created_at,
+    updated_at: userCard.created_at,
+    birth: userCard.birth,
+    sex: userCard.sex,
+  };
+};
+
+export default formatAuthorRes;
